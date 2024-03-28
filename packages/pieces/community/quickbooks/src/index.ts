ddad3ebcff67a28439586ca6fb33b72a843f5750
kumar vikram <kumarvikramshahi@gmail.com>
2024-03-28 10:58:58 +0530

    feat: quickbook piece WIP

---
+++ packages/pieces/community/quickbooks/src/index.ts
@@ -0,0 +1,29 @@
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { createACustomer } from './lib/actions/create-a-customer';
+import { newCustomerCreated } from './lib/triggers/new-customer-created';
+
+export const quickbooksAuth = PieceAuth.OAuth2({
+  description: '',
+
+  authUrl: 'https://appcenter.intuit.com/app/connect/oauth2/',
+  tokenUrl: 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
+  required: true,
+  scope: [
+    'com.intuit.quickbooks.accounting',
+    'openid',
+    'profile',
+    'email',
+    'phone',
+    'address',
+  ],
+});
+
+export const quickbooks = createPiece({
+  displayName: 'QuickBooks',
+  auth: quickbooksAuth,
+  minimumSupportedRelease: '0.20.0',
+  logoUrl: 'https://cdn.activepieces.com/pieces/quickbooks.png',
+  authors: ['kumarvikramshahi'],
+  actions: [createACustomer],
+  triggers: [newCustomerCreated],
+});
