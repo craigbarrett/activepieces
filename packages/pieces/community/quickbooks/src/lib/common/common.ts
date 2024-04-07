96ba320eb565d35f2047b9762f606aded9380732
kumar vikram <kumarvikramshahi@gmail.com>
2024-04-07 20:11:30 +0530

    fix: saving realmId and verifierToken WIP

--- packages/pieces/community/quickbooks/src/lib/common/common.ts
+++ packages/pieces/community/quickbooks/src/lib/common/common.ts
@@ -1,20 +1,45 @@
-import { StoreScope, Store } from '@activepieces/pieces-framework';
+import {
+  StoreScope,
+  Store,
+  Property,
+  Validators,
+} from '@activepieces/pieces-framework';
 
 export const quickbooksCommons = {
-  baseUrl: 'https://sandbox-quickbooks.api.intuit.com/v3/company',
-  getRealmId: async (store: Store, realmId: string | undefined) => {
-    let getRealmId = await store.get<string>('realmId', StoreScope.PROJECT);
-    if (!getRealmId) {
-      if (!realmId) {
-        throw new Error('Please provide realmId/companyId to create customer');
+  baseUrl: 'https://sandbox-quickbooks.api.intuit.com',
+  getKeyValue: async (
+    store: Store,
+    clientId: string,
+    key: string,
+    value: string | undefined
+  ) => {
+    let getValue = await store.get<string>(clientId + key, StoreScope.PROJECT);
+    console.log('key: ', clientId + key);
+    if (!getValue) {
+      if (!value) {
+        throw new Error('No value passed to store');
       }
-      await store.put('realmId', realmId, StoreScope.PROJECT);
-      getRealmId = realmId
-    } else if(getRealmId && realmId){
-      await store.delete('realmId', StoreScope.PROJECT);
-      await store.put('realmId', realmId, StoreScope.PROJECT);
-      getRealmId = realmId
+      await store.put(clientId + key, value, StoreScope.PROJECT);
+      getValue = value;
+    } else if (getValue && value) {
+      await store.delete(clientId + key, StoreScope.PROJECT);
+      await store.put(clientId + key, value, StoreScope.PROJECT);
+      getValue = value;
     }
-    return getRealmId;
+    console.log('getVal: ', getValue);
+    return getValue;
   },
+  realmId: Property.LongText({
+    displayName: 'Realm ID/ Company ID',
+    required: false,
+    description:
+      'Enter Company Id/realm ID that can be obtained can be obtained by visiting https://developer.intuit.com/app/developer/playground',
+    validators: [Validators.maxLength(20)],
+  }),
+  webhookVerifierToken: Property.LongText({
+    displayName: 'Webhook Verifier Token',
+    required: false,
+    description:
+      'Enter Webhook Verifier Token, can be obtained by visiting https://developer.intuit.com/app/developer/dashboard then "App Name" => "Production Settings" => Webhooks and then entering your Public-URL of Ngrok',
+  }),
 };
